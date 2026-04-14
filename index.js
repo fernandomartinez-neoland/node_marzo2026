ac99f8d53dcd5419210aa55f692aadd689cf8442
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-14 16:02:19 +0200

    conectado a la bd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hola Mundo");
+import db from 'mongoose';
+
+db.connect('mongodb+srv://usuario_prueba:123@cluster0.w6ghmta.mongodb.net/')
+.then(() => {
+    console.log('Conexión exitosa a MongoDB');
+})
+.catch((error) => {
+    console.error('Error al conectar a MongoDB:', error);
+});
