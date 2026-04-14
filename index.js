76179ef470a0d20a5a8c40159b924aa8192d83b4
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-14 16:53:37 +0200

    find

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import db from 'mongoose';
 
-db.connect('mongodb+srv://usuario_prueba:123@cluster0.w6ghmta.mongodb.net/')
+await db.connect('mongodb+srv://usuario_prueba:123@cluster0.w6ghmta.mongodb.net/marzo2026')
 .then(() => {
     console.log('Conexión exitosa a MongoDB');
 })
 .catch((error) => {
     console.error('Error al conectar a MongoDB:', error);
 });
+
+const Schema = db.Schema;
+
+const users = new Schema({
+  nombre: String,
+  apellido: String,
+  email: String,
+  date:Date
+});
+
+const userModel= db.model('users', users, 'users');
+
+console.log(await userModel.find({}));
